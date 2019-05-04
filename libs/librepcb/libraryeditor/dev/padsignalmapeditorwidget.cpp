2d5dff4cda950465ca1694b7a0693f0df062d792
U. Bruhin <urbibruhin@bluewin.ch>
2019-05-05 01:00:35 +0200

    Refactor PadSignalMapEditorWidget

--- libs/librepcb/libraryeditor/dev/padsignalmapeditorwidget.cpp
+++ libs/librepcb/libraryeditor/dev/padsignalmapeditorwidget.cpp
@@ -22,9 +22,9 @@
  ******************************************************************************/
 #include "padsignalmapeditorwidget.h"
 
-#include <librepcb/common/toolbox.h>
-#include <librepcb/common/undostack.h>
-#include <librepcb/library/dev/cmd/cmddevicepadsignalmapitemedit.h>
+#include <librepcb/common/model/comboboxdelegate.h>
+#include <librepcb/common/model/sortfilterproxymodel.h>
+#include <librepcb/library/dev/devicepadsignalmapmodel.h>
 
 #include <QtCore>
 #include <QtWidgets>
@@ -42,36 +42,34 @@ namespace editor {
 
 PadSignalMapEditorWidget::PadSignalMapEditorWidget(QWidget* parent) noexcept
   : QWidget(parent),
-    mTable(new QTableWidget(this)),
-    mUndoStack(nullptr),
-    mPadSignalMap(nullptr) {
-  mTable->setCornerButtonEnabled(false);
-  mTable->setSelectionBehavior(QAbstractItemView::SelectRows);
-  mTable->setSelectionMode(QAbstractItemView::SingleSelection);
-  mTable->setWordWrap(false);  // avoid too high cells due to word wrap
-  mTable->setColumnCount(_COLUMN_COUNT);
-  mTable->setHorizontalHeaderItem(COLUMN_PAD,
-                                  new QTableWidgetItem(tr("Pad Name")));
-  mTable->setHorizontalHeaderItem(COLUMN_SIGNAL,
-                                  new QTableWidgetItem(tr("Component Signal")));
-  mTable->horizontalHeader()->setSectionResizeMode(COLUMN_PAD,
-                                                   QHeaderView::Stretch);
-  mTable->horizontalHeader()->setSectionResizeMode(COLUMN_SIGNAL,
-                                                   QHeaderView::Stretch);
-  mTable->verticalHeader()->setSectionResizeMode(QHeaderView::Fixed);
-  mTable->verticalHeader()->setMinimumSectionSize(20);
-  connect(mTable, &QTableWidget::currentCellChanged, this,
-          &PadSignalMapEditorWidget::currentCellChanged);
+    mView(new QTableView(this)),
+    mModel(new DevicePadSignalMapModel(this)),
+    mProxy(new SortFilterProxyModel(this)) {
+  mProxy->setSourceModel(mModel.data());
+  mView->setModel(mProxy.data());
+  mView->setAlternatingRowColors(true);  // increase readability
+  mView->setCornerButtonEnabled(false);  // not needed
+  mView->setSelectionBehavior(QAbstractItemView::SelectRows);
+  mView->setSelectionMode(QAbstractItemView::SingleSelection);
+  mView->setEditTriggers(QAbstractItemView::AllEditTriggers);
+  mView->setSortingEnabled(true);
+  mView->setWordWrap(false);  // avoid too high cells due to word wrap
+  mView->verticalHeader()->setMinimumSectionSize(10);  // more compact rows
+  mView->verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+  mView->horizontalHeader()->setSectionResizeMode(
+      DevicePadSignalMapModel::COLUMN_PAD, QHeaderView::ResizeToContents);
+  mView->horizontalHeader()->setSectionResizeMode(
+      DevicePadSignalMapModel::COLUMN_SIGNAL, QHeaderView::Stretch);
+  mView->setItemDelegateForColumn(DevicePadSignalMapModel::COLUMN_SIGNAL,
+                                  new ComboBoxDelegate(this));
+  mView->sortByColumn(DevicePadSignalMapModel::COLUMN_PAD, Qt::AscendingOrder);
 
   QVBoxLayout* layout = new QVBoxLayout(this);
   layout->setContentsMargins(0, 0, 0, 0);
-  layout->addWidget(mTable);
-
-  updateTable();
+  layout->addWidget(mView.data());
 }
 
 PadSignalMapEditorWidget::~PadSignalMapEditorWidget() noexcept {
-  setReferences(nullptr, nullptr);
 }
 
 /*******************************************************************************
@@ -80,207 +78,17 @@ PadSignalMapEditorWidget::~PadSignalMapEditorWidget() noexcept {
 
 void PadSignalMapEditorWidget::setReferences(UndoStack*          undoStack,
                                              DevicePadSignalMap* map) noexcept {
-  if (mPadSignalMap) {
-    mPadSignalMap->unregisterObserver(this);
-    for (const DevicePadSignalMapItem& item : *mPadSignalMap) {
-      disconnect(&item, &DevicePadSignalMapItem::signalUuidChanged, this,
-                 &PadSignalMapEditorWidget::updateTable);
-    }
-  }
-  mUndoStack    = undoStack;
-  mPadSignalMap = map;
-  if (mPadSignalMap) {
-    mPadSignalMap->registerObserver(this);
-    for (const DevicePadSignalMapItem& item : *mPadSignalMap) {
-      connect(&item, &DevicePadSignalMapItem::signalUuidChanged, this,
-              &PadSignalMapEditorWidget::updateTable);
-    }
-  }
-  updateTable();
+  mModel->setPadSignalMap(map);
+  mModel->setUndoStack(undoStack);
 }
 
 void PadSignalMapEditorWidget::setPadList(const PackagePadList& list) noexcept {
-  mPads = list;
-  updateTable();
+  mModel->setPadList(list);
 }
 
 void PadSignalMapEditorWidget::setSignalList(
     const ComponentSignalList& list) noexcept {
-  mSignals = list;
-  updateTable();
-}
-
-/*******************************************************************************
- *  Private Slots
- ******************************************************************************/
-
-void PadSignalMapEditorWidget::currentCellChanged(int currentRow,
-                                                  int currentColumn,
-                                                  int previousRow,
-                                                  int previousColumn) noexcept {
-  Q_UNUSED(currentColumn);
-  Q_UNUSED(previousRow);
-  Q_UNUSED(previousColumn);
-  mSelectedPad = getPadUuidOfRow(currentRow);
-}
-
-void PadSignalMapEditorWidget::componentSignalChanged(int index) noexcept {
-  QComboBox* cbx = dynamic_cast<QComboBox*>(sender());
-  Q_ASSERT(cbx);
-  tl::optional<Uuid> pad = getPadUuidOfTableCellWidget(sender());
-  if (pad) {
-    tl::optional<Uuid> signal =
-        Uuid::tryFromString(cbx->itemData(index, Qt::UserRole).toString());
-    if (mUndoStack && mPadSignalMap) setComponentSignal(*pad, signal);
-  }
-}
-
-/*******************************************************************************
- *  Observer
- ******************************************************************************/
-
-void PadSignalMapEditorWidget::listObjectAdded(
-    const DevicePadSignalMap& list, int newIndex,
-    const std::shared_ptr<DevicePadSignalMapItem>& ptr) noexcept {
-  Q_UNUSED(list);
-  Q_UNUSED(newIndex);
-  Q_ASSERT(&list == mPadSignalMap);
-  connect(ptr.get(), &DevicePadSignalMapItem::signalUuidChanged, this,
-          &PadSignalMapEditorWidget::updateTable);
-  updateTable();
-}
-
-void PadSignalMapEditorWidget::listObjectRemoved(
-    const DevicePadSignalMap& list, int oldIndex,
-    const std::shared_ptr<DevicePadSignalMapItem>& ptr) noexcept {
-  Q_UNUSED(list);
-  Q_UNUSED(oldIndex);
-  Q_ASSERT(&list == mPadSignalMap);
-  disconnect(ptr.get(), &DevicePadSignalMapItem::signalUuidChanged, this,
-             &PadSignalMapEditorWidget::updateTable);
-  updateTable();
-}
-
-/*******************************************************************************
- *  Private Methods
- ******************************************************************************/
-
-void PadSignalMapEditorWidget::updateTable() noexcept {
-  blockSignals(true);
-
-  // remove all rows
-  int scrollbarValue = mTable->verticalScrollBar()->value();
-  int selectedRow    = -1;
-  mTable->clearSelection();
-  mTable->clearContents();
-
-  if (mUndoStack && mPadSignalMap) {
-    mTable->setEnabled(true);
-
-    // list all pads
-    mTable->setRowCount(mPadSignalMap->count());
-    for (int row = 0; row < mPads.count(); ++row) {
-      const PackagePad& pad = *mPads.at(row);
-      setTableRowContent(row, pad.getUuid(), *pad.getName(),
-                         DevicePadSignalMapHelpers::tryGetSignalUuid(
-                             *mPadSignalMap, pad.getUuid()));
-      if (pad.getUuid() == mSelectedPad) {
-        selectedRow = row;
-      }
-    }
-
-    // workaround to trigger column resizing because sometimes auto-resizing
-    // does not work
-    mTable->hide();
-    mTable->show();
-
-    // order by pad name
-    mTable->sortByColumn(COLUMN_PAD, Qt::AscendingOrder);
-
-    // set selected row
-    mTable->verticalScrollBar()->setValue(scrollbarValue);
-    mTable->selectRow(selectedRow);
-    mSelectedPad = getPadUuidOfRow(selectedRow);
-  } else {
-    mTable->setEnabled(false);
-  }
-
-  blockSignals(false);
-}
-
-void PadSignalMapEditorWidget::setTableRowContent(
-    int row, const Uuid& padUuid, const QString& padName,
-    const tl::optional<Uuid>& signalUuid) noexcept {
-  // header
-  QTableWidgetItem* headerItem = new QTableWidgetItem(padUuid.toStr());
-  QFont             headerFont = headerItem->font();
-  headerFont.setStyleHint(
-      QFont::Monospace);  // ensure that the column width is fixed
-  headerFont.setFamily("Monospace");
-  headerItem->setFont(headerFont);
-  mTable->setVerticalHeaderItem(row, headerItem);
-
-  // pad
-  QTableWidgetItem* padItem = new QTableWidgetItem();
-  padItem->setFlags(padItem->flags() & ~Qt::ItemFlags(Qt::ItemIsEditable));
-  padItem->setData(Qt::DisplayRole, Toolbox::stringOrNumberToQVariant(padName));
-  padItem->setData(Qt::UserRole, padUuid.toStr());
-  mTable->setItem(row, COLUMN_PAD, padItem);
-
-  // signal
-  int        cbxHeight = 23;  // TODO: can we determine this value dynamically?
-  QComboBox* signalComboBox = new QComboBox(this);
-  signalComboBox->setProperty("pad", padUuid.toStr());
-  signalComboBox->setStyleSheet(
-      "padding: 0px 3px 0px 3px;");  // reduce required space
-  signalComboBox->setFixedHeight(cbxHeight);
-  for (const ComponentSignal& sig : mSignals) {
-    signalComboBox->addItem(*sig.getName(), sig.getUuid().toStr());
-    // Set display role to a QVariant to get numbers sorted by value and strings
-    // alphabetically.
-    signalComboBox->setItemData(
-        signalComboBox->count() - 1,
-        Toolbox::stringOrNumberToQVariant(*sig.getName()), Qt::DisplayRole);
-  }
-  signalComboBox->model()->sort(0);
-  signalComboBox->insertItem(0, tr("(not connected)"));
-  int currentIndex =
-      signalUuid ? signalComboBox->findData(signalUuid->toStr(), Qt::UserRole)
-                 : -1;
-  signalComboBox->setCurrentIndex(currentIndex > 0 ? currentIndex : 0);
-  connect(
-      signalComboBox,
-      static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
-      this, &PadSignalMapEditorWidget::componentSignalChanged);
-  mTable->setCellWidget(row, COLUMN_SIGNAL, signalComboBox);
-
-  // adjust the height of the row according to the size of the contained widgets
-  mTable->verticalHeader()->resizeSection(row, cbxHeight);
-}
-
-void PadSignalMapEditorWidget::setComponentSignal(
-    const Uuid& pad, const tl::optional<Uuid>& signal) noexcept {
-  try {
-    DevicePadSignalMapItem& item = *mPadSignalMap->get(pad);  // can throw
-    QScopedPointer<CmdDevicePadSignalMapItemEdit> cmd(
-        new CmdDevicePadSignalMapItemEdit(item));
-    cmd->setSignalUuid(signal);
-    mUndoStack->execCmd(cmd.take());
-  } catch (const Exception& e) {
-    QMessageBox::critical(this, tr("Could not set signal"), e.getMsg());
-  }
-}
-
-tl::optional<Uuid> PadSignalMapEditorWidget::getPadUuidOfTableCellWidget(
-    QObject* obj) const noexcept {
-  return Uuid::tryFromString(obj->property("pad").toString());
-}
-
-tl::optional<Uuid> PadSignalMapEditorWidget::getPadUuidOfRow(int row) const
-    noexcept {
-  QTableWidgetItem* item = mTable->item(row, COLUMN_PAD);
-  return item ? Uuid::tryFromString(item->data(Qt::UserRole).toString())
-              : tl::nullopt;
+  mModel->setSignalList(list);
 }
 
 /*******************************************************************************
--- libs/librepcb/libraryeditor/dev/padsignalmapeditorwidget.h
+++ libs/librepcb/libraryeditor/dev/padsignalmapeditorwidget.h
@@ -36,12 +36,16 @@
 namespace librepcb {
 
 class UndoStack;
+class SortFilterProxyModel;
 
 namespace workspace {
 class Workspace;
 }
 
 namespace library {
+
+class DevicePadSignalMapModel;
+
 namespace editor {
 
 /*******************************************************************************
@@ -50,16 +54,9 @@ namespace editor {
 
 /**
  * @brief The PadSignalMapEditorWidget class
- *
- * @author ubruhin
- * @date 2017-03-25
  */
-class PadSignalMapEditorWidget final : public QWidget,
-                                       private DevicePadSignalMap::IF_Observer {
+class PadSignalMapEditorWidget final : public QWidget {
   Q_OBJECT
-
-private:  // Types
-  enum Column { COLUMN_PAD = 0, COLUMN_SIGNAL, _COLUMN_COUNT };
 
 public:
   // Constructors / Destructor
@@ -76,35 +73,10 @@ public:
   PadSignalMapEditorWidget& operator=(const PadSignalMapEditorWidget& rhs) =
       delete;
 
-private:  // Slots
-  void currentCellChanged(int currentRow, int currentColumn, int previousRow,
-                          int previousColumn) noexcept;
-  void componentSignalChanged(int index) noexcept;
-
-private:  // Observer
-  void listObjectAdded(
-      const DevicePadSignalMap& list, int newIndex,
-      const std::shared_ptr<DevicePadSignalMapItem>& ptr) noexcept override;
-  void listObjectRemoved(
-      const DevicePadSignalMap& list, int oldIndex,
-      const std::shared_ptr<DevicePadSignalMapItem>& ptr) noexcept override;
-
-private:  // Methods
-  void updateTable() noexcept;
-  void setTableRowContent(int row, const Uuid& padUuid, const QString& padName,
-                          const tl::optional<Uuid>& signalUuid) noexcept;
-  void setComponentSignal(const Uuid&               pad,
-                          const tl::optional<Uuid>& signal) noexcept;
-  tl::optional<Uuid> getPadUuidOfTableCellWidget(QObject* obj) const noexcept;
-  tl::optional<Uuid> getPadUuidOfRow(int row) const noexcept;
-
-private:  // Data
-  QTableWidget*       mTable;
-  UndoStack*          mUndoStack;
-  DevicePadSignalMap* mPadSignalMap;
-  PackagePadList      mPads;
-  ComponentSignalList mSignals;
-  tl::optional<Uuid>  mSelectedPad;
+private:
+  QScopedPointer<QTableView>              mView;
+  QScopedPointer<DevicePadSignalMapModel> mModel;
+  QScopedPointer<SortFilterProxyModel>    mProxy;
 };
 
 /*******************************************************************************
